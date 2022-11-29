d1117fb61e02986c4e39951868aaeb9fe5823d9f
IrinchinovZh <exp.bur@gmail.com>
2022-11-29 13:26:55 +0800

    Add zad19

---
+++ Zad19/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.Clear();
+Console.WriteLine("Vvedite patizhnachnoe chislo: ");
+int n=Convert.ToInt32(Console.ReadLine());
+int a=n/1000;
+//Console.WriteLine(a);
+int b=n%10;
+//Console.WriteLine(b);
+int c=(n/10)%10+b*10;
+
+//Console.WriteLine(c);
+
+if(c==a)
+Console.WriteLine("Chislo avlatsa Palindromom");
+else
+Console.WriteLine("Chislo ne avlatsa Palindromom");
